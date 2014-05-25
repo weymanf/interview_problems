5ca5c9ece7c72c0cecced8a033132c53c1823f62
Weyman Fung <wfung0083@yahoo.com>
2014-05-24 19:00:46 -0700

    start linklists

---
+++ link_list.rb
@@ -0,0 +1,25 @@
+#build a linklists
+
+class linklist
+
+  def start
+
+
+  end
+
+
+
+
+end
+
+
+class link_item
+  attr_accessor :value, :nextt
+
+  def initialize(value, nextt = nil)
+    @value = value
+    @nextt = nil
+  end
+
+
+end
