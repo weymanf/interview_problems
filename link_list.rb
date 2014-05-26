b988d5971f0b98aa34c09906f69ae3361fd06aeb
Weyman Fung <wfung0083@yahoo.com>
2014-05-25 18:13:59 -0700

    linklists too strong

--- link_list.rb
+++ link_list.rb
@@ -2,21 +2,38 @@
 
 class linklist
 
-  def start
+  attr_accessor :start
 
-
+  def initialize(link_item)
+    @start = link_item
   end
 
 
+  def add(node)
+    current_link_item = @start
+    while current_node
+      current_link_item = current_link_item.next
 
+
+    end
+    current_link_item.next = node
+  end
+
+  def find
+    
+  end
+
+  def delete(nth_link_item)
+
+  end
 
 end
 
 
 class link_item
-  attr_accessor :value, :nextt
+  attr_accessor :value, :next
 
-  def initialize(value, nextt = nil)
+  def initialize(value, next = nil)
     @value = value
     @nextt = nil
   end
