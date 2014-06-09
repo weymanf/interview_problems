53178e7b41056ba74efd0269a4dd9ca47a4b896b
Weyman Fung <wfung0083@yahoo.com>
2014-06-09 13:39:05 -0700

    more problems. Max Substrings. Good nodes.

---
+++ good_nodes.rb
@@ -0,0 +1,57 @@
+=begin
+To satisfy. A good node must satisfy one of these qualifications
+- It's node #1
+- It's pointing to node #1
+- It's point to a good node
+
+The first line is a integer N telling how many nodes there are.
+The next lines are the Nodes with integer N telling which nodes they are pointed to.
+Output the minimum number of changes of nodes to make them all good nodes.
+
+Output:
+Print an integer which is the answer to the problem
+Sample Input 1:
+5
+1
+2
+3
+4
+5
+Sample output 1:
+4
+Explanation:
+We have to change pointers for four nodes (node #2 to node #5) to point to node #1. Thus 4 changes are required
+Sample input 2:
+5
+5
+5
+5
+5
+5
+Sample output 2:
+1
+Explanation:
+We have to just change node #5 to point to node #1 (tail node) which will make node #5 good. Since all the other nodes point to a good node (node #5), every node becomes a good node.
+
+
+=end
+
+nodes_hash = {}
+count = 0
+
+$stdin.each_with_index do |line, index|
+   nodes_hash[index] = line.to_i
+end
+
+for i in (2...nodes_hash[0])
+  if nodes_hash[i] == 1 || nodes_hash[nodes_hash[i]] == 1
+    next
+  
+
+
+end
+
+
+
+
+
---
+++ num_of_substrings.rb
@@ -0,0 +1,41 @@
+=begin
+
+You are given a string S. You need to compute the number of distinct substrings of S.                  
+A substring of S can be obtained by either removing one or more characters from the left side of S, or by removing one or more characters from the right side of S, or a combination of both.
+Input Format:
+There is only one line of the input and it contains the string S
+S will only be composed of characters 'a' to 'z' and will not have more than 10^5 characters.
+Output Format:
+You should print the number of distinct substrings of S to the output. This number might be larger than what can be permitted by 32-bit integers.
+Sample input:
+kincenvizh
+Sample output:
+53
+=end
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+S = gets.chomp()
+
+=begin
+str_arr = S.split(//)
+
+#naive solution. build the substrings and count them
+len = (1..str_arr.length).to_a.flat_map do |n|
+        str_arr.each_cons(n).to_a
+      end.uniq.length
+
+print len
+=end
+
+
+
+length_of_ar = S.length
+length_of_uniq = S.uniq.length
+
+#number of substrings is added 
+#minus the duplicated letters
+len = (1..S).inject(:+) - (length_of_ar - length_of_uniq)
+
+
+print "#{len}"
